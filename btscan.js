01c4f0a4b4d50a54e677fd4c22bd0f30ec6d3518
Pedro Paixao <PaixaoP@gmail.com>
2015-04-29 04:20:51 -0400

    refactor to use events

--- btscan.js
+++ btscan.js
@@ -3,18 +3,6 @@
  * Scan for bluetooth devices and, if found, send a message to
  * to MQTT Broker's topic
  *
- * Message format is:
- * 
- *  <Module Name>,<device type>,<MAC Addr>,<BT device Name>
- *
- * Module name is the name by which the computer running this script is known
- * on the network. For instance 'MasterBedroom'. Module names cannot have ','
- * or any other special character. Onlyletters, numbers and spaces are allowed.
- *
- * Device type is 'bluetooth'
- * MAC Addr is the device's MAC address
- * BT device name is the bluetooth device's name
- *
  * Author: Pedro Paixao
  * License: MIT
  */
@@ -23,7 +11,9 @@ var fs = require('fs');
 
 var Scanner = require("bluetooth-scanner");
 var cli = require('commander');
+
 var options = {};
+
 cli
   .version('1.0.0')
   .usage('[options]')
@@ -35,13 +25,13 @@ cli
   .option('-h, --host <host>',
           'MQTT Broker host name or IP address')
   .option('-p, --port [n]',
-          'port the MQTT Broker is listening on. Defaults to 8883',
+          'port the MQTT Broker is listening on. Defaults to 1883',
           parseInt,
-          8883)
+          1883)
   .option('-c, --cafile [file]',
           'path to a file containing trusted CA certificates to enable encrypted communication.')
-  .option('-n, --name [room]',
-          'name of the module running this scrip. Defaults to room',
+  .option('-i, --id [room]',
+          'client Id of the module running this scrip. Defaults to room',
           'room')
   .option('-t, --topic [topic]',
           'MQTT topic to publish messages to. Defaults to device/presence',
@@ -49,7 +39,7 @@ cli
   .option('-D, --device_type [type]',
           'type of devices that are scanned for. Defaults to bluetooth',
           'bluetooth')
-  .option('-i, --interface [interface]',
+  .option('-I, --interface [interface]',
           'bluetooth interface to user. Defaults to hci0',
           'hci0')
   .option('-T, --timeout <n>',
@@ -57,8 +47,8 @@ cli
           parseInt,
           0)
   .option('--protocol [proto]',
-          'MQTT protocol to use please see MQTT.js help for details. Defaults to mqtts',
-          'mqtts')
+          'MQTT protocol to use please see MQTT.js help for details. Defaults to mqtt',
+          'mqtt')
   .option('--no-scan', 'do not scan for devices, just connect to MQTT broker')
   .option('--no-unauthorized',
           'if true self signed certificates will be rejected and you need to pass a cafile' +
@@ -83,15 +73,15 @@ cli
   
   cli.parse(process.argv);
   
+
 if (!cli.host) {
   console.log('please specify host of the mqtt broker');
   cli.outputHelp();
   process.exit();
 }
 
-if (!cli.help) {
-  require("console-stamp")(console, "HH:MM:ss.l", '[' + process.pid + ']');
-}
+
+require("console-stamp")(console, "HH:MM:ss.l", '[' + process.pid + ']');
 
 options.host = cli.host;
 options.port = cli.port;
@@ -135,7 +125,7 @@ console.log('using interface:', cli.interface);
 
 console.log('device type:', cli.device_type);
 console.log('mqtt topic:', cli.topic);
-console.log('name:', cli.name);
+console.log('client id:', cli.id);
 
 if (cli.cafile) {
   console.log('CA certificate:', cli.cafile);
@@ -160,49 +150,84 @@ else {
 }
 options.rejectUnauthorized = cli.unauthorized;
 
-var scanner;
 
 console.log('connecting...');
-console.time( "MyConnectTimer" );
+console.time( "MQTT connection time" );
 var client = mqtt.connect(options);
 
 client.on('connect', function () {
   console.log('MQTT Broker Conncetion OK');
-  console.timeEnd("MyConnectTimer");
-  var msg = cli.name + "," + cli.device_type + "," + mac + "," + name
-  client.publish(cli.topic, cli.name + "," + cli.device_type + ",scan start");
-  
+  console.timeEnd("MQTT connection time");
   
   if (cli.scan) {
-    console.log('starting scan...');
+    
+    client.publish(cli.topic, {
+      name: cli.id,
+      device_type: cli.device_type,
+      command: 'scan start'
+    });
     
     if (cli.timeout) {
+      
       setTimeout(function() {
-        client.publish(cli.topic, cli.name + "," + cli.device_type + ",scan timeout");
+        
+        client.publish(cli.topic, {
+          id: cli.id,
+          device_type: cli.device_type,
+          command: 'scan timeout'
+        });
+        
         console.log('Timeout. Terminating scanner.');
+        
         if (scanner) {
           scanner.destroy;  
         }
+        
         process.exit();
+      
       }, cli.timeout);
+      
     }
 
     // Start Scan
-    scanner = new Scanner(cli.interface,function(mac, name) {
-      // <Module Name>,<device type>,<MAC Addr>,<BT device Name>
-      var msg = cli.name + ',' + cli.device_type + ',' + mac + ',' + name;
+    var scanner = new Scanner(cli.interface);
+    
+    scanner.on('device', function(result) {
+      var mac = result[1];
+      var name = result[2];
+
+      // <Module Id>,<device type>,<MAC Addr>,<BT device Name>
+      var msg = cli.id + ',' + cli.device_type + ',' + mac + ',' + name;
+      
       if (cli.debug) {
         console.log('publish message. Topic: ' + cli.topic + ' - ' + msg);
       }
-      client.publish(cli.topic, msg);
+      
+      client.publish(cli.topic, {
+        id: cli.id,
+        device_type: cli.device_type,
+        mac: mac,
+        name: name,
+        command: 'scan timeout'
+      });
+      
     });
+    
+    scanner.on('done', function(message) {
+      console.log('scanner exit:', message);
+      process.exit();
+    });
+    
+    scanner.on('error', function(error) {
+      console.log(error);
+      process.exit();
+    });
+    
   }
   else {
     console.log('not scanning for devices...');
   }
 });
-
-
 
 client.on('message', function(topic, message) {
   console.log('Message:', topic, message.toString('ascii'));
@@ -210,6 +235,7 @@ client.on('message', function(topic, message) {
 
 client.on('error', function(error) {
   console.log('MQTT Broker ' + error.toString());
+  process.exit();
 });
 
 client.on('close', function(message) {
@@ -218,6 +244,6 @@ client.on('close', function(message) {
 });
 
 client.on('offline', function(message) {
-  console.log('MQTT Broker connection failed');
+  console.log('going offline');
   process.exit();
 });
